038a200760d209dd9d117a4ddf0ccc1dad35d8df
John(Jack) Jamison <37374979+jackjohnjamison@users.noreply.github.com>
2023-03-20 00:36:24 +0000

    Update npc.js

--- src/lib/entities/npc.js
+++ src/lib/entities/npc.js
@@ -1,44 +1,42 @@
-import { isWalkable } from "../map";
-import { entity } from "./entity";
-import { randomVariationInt } from "../utilities";
-import { pathfinding } from "./movement/pathfinding";
-import { npcPatrolSpeed } from "../constants";
+import { isWalkable } from '../map'
+import { entity } from './entity'
+import { randomVariationInt } from '../utilities'
+import { pathfinding } from './movement/pathfinding'
+import { npcPatrolSpeed } from '../constants'
 import { modeStore } from '@/stores/mode'
 
 class npc extends entity {
   constructor(sprite) {
-    super(sprite);
-    this.path = [];
+    super(sprite)
+    this.path = []
 
-    this.pathFinder = pathfinding(this, npcPatrolSpeed);
+    this.pathFinder = pathfinding(this, npcPatrolSpeed)
   }
 
   pickPath = () => {
-    const { pathFinder, tileIndex } = this;
+    const { pathFinder, tileIndex } = this
 
     const destination = {
       x: randomVariationInt(tileIndex.x, 10),
-      y: randomVariationInt(tileIndex.y, 10),
-    };
+      y: randomVariationInt(tileIndex.y, 10)
+    }
 
     if (isWalkable(destination)) {
-      pathFinder.requestMove(destination);
+      pathFinder.requestMove(destination)
     }
-  };
+  }
 
   update = (delta) => {
-    const { pathFinder, pickPath, redraw, isMoving } = this;
-
     // This is super ugly! I should probably stop the whole loop of NPC actions instead of testing every time!
-    if (modeStore().mode !== "editMode") {
+    if (modeStore().mode !== 'editMode') {
+      const { pathFinder, pickPath, redraw, isMoving } = this
       if (!isMoving) {
-        pickPath();
+        pickPath()
       }
-      pathFinder.move(delta);
+      pathFinder.move(delta)
+      redraw()
     }
-
-    redraw();
-  };
+  }
 }
 
-export { npc };
+export { npc }
