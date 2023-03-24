5821b05c46e03c07904a515a06b8fec11bae672a
jjamisonMadgex <37374979+jackjohnjamison@users.noreply.github.com>
2023-03-24 13:56:52 +0000

    Tidy

--- src/lib/scene/on-frame-functions.js
+++ src/lib/scene/on-frame-functions.js
@@ -1,22 +1,22 @@
-import { scene } from ".";
-import { renderFrame } from "./render-frame";
+import { scene } from '.'
+import { renderFrame } from './render-frame'
 
 // For additional functions like the frame rate monitor
 const additionalFunctions = []
 
 // Functions fed into the render loop
 const onFrameFunctions = (delta) => {
-  const { onFrameControls } = scene;
+  const { onFrameControls } = scene
 
   if (onFrameControls) {
-    onFrameControls(delta);
+    onFrameControls(delta)
   }
 
   additionalFunctions.forEach((func) => {
     func(delta)
   })
 
-  renderFrame(delta);
-};
+  renderFrame(delta)
+}
 
-export { onFrameFunctions, additionalFunctions };
+export { onFrameFunctions, additionalFunctions }
--- src/lib/scene/set-view.js
+++ src/lib/scene/set-view.js
@@ -1,5 +1,5 @@
-import { scene } from ".";
-import { tileWidth, tileHeight, paddingBottom, paddingTop } from "../constants";
+import { scene } from '.'
+import { tileWidth, tileHeight, paddingBottom, paddingTop } from '../constants'
 
 const setView = ({ xTiles, yTiles }) => {
   const {
@@ -11,61 +11,58 @@ const setView = ({ xTiles, yTiles }) => {
     floorCanvas,
     canvasMid,
     entityCanvas,
-    canvasTop,
-  } = scene;
+    canvasTop
+  } = scene
 
-  const baseHeight = ((xTiles + yTiles) / 2) * tileHeight;
+  const baseHeight = ((xTiles + yTiles) / 2) * tileHeight
 
   // Canvas dimensions once padding is included
-  const width = ((xTiles + yTiles) / 2) * tileWidth;
-  const height = baseHeight + paddingBottom + paddingTop;
+  const width = ((xTiles + yTiles) / 2) * tileWidth
+  const height = baseHeight + paddingBottom + paddingTop
 
   // Sets floor and entity canvas size
-  floorCanvas.width = entityCanvas.width = width;
-  floorCanvas.height = entityCanvas.height = height;
+  floorCanvas.width = entityCanvas.width = width
+  floorCanvas.height = entityCanvas.height = height
 
   // The exactly fits the grid in the center on the Y axis
   const baseOriginY =
-    baseHeight / 2 -
-    (tileHeight / 4) * (yTiles - xTiles) -
-    tileHeight / 2 +
-    paddingTop;
+    baseHeight / 2 - (tileHeight / 4) * (yTiles - xTiles) - tileHeight / 2 + paddingTop
 
   // Used to caculate the position of the grid relative to the canvas
   const origin = {
     x: 0,
-    y: baseOriginY,
-  };
+    y: baseOriginY
+  }
 
   const translate = {
     x: 0,
-    y: 0,
-  };
+    y: 0
+  }
 
   const setApertureSize = () => {
-    canvasMid.width = canvasTop.width = canvasRoot.clientWidth;
-    canvasMid.height = canvasTop.height = canvasRoot.clientHeight;
+    canvasMid.width = canvasTop.width = canvasRoot.clientWidth
+    canvasMid.height = canvasTop.height = canvasRoot.clientHeight
 
-    ctxMid.setTransform(1, 0, 0, 1, translate.x, translate.y);
-    ctxMid.imageSmoothingEnabled = false;
-    ctxTop.setTransform(1, 0, 0, 1, translate.x, translate.y);
-    ctxTop.imageSmoothingEnabled = false;
+    ctxMid.setTransform(1, 0, 0, 1, translate.x, translate.y)
+    ctxMid.imageSmoothingEnabled = false
+    ctxTop.setTransform(1, 0, 0, 1, translate.x, translate.y)
+    ctxTop.imageSmoothingEnabled = false
 
-    floorCtx.imageSmoothingEnabled = false;
-    entityCtx.imageSmoothingEnabled = false;
-  };
+    floorCtx.imageSmoothingEnabled = false
+    entityCtx.imageSmoothingEnabled = false
+  }
 
   onresize = () => {
-    setApertureSize();
-  };
+    setApertureSize()
+  }
 
   return {
     origin,
     xTiles,
     yTiles,
     translate,
-    setApertureSize,
-  };
-};
+    setApertureSize
+  }
+}
 
-export { setView };
+export { setView }
