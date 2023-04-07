b87208a5e61c5f97ec02818481f113cb59ab7304
jjamisonMadgex <37374979+jackjohnjamison@users.noreply.github.com>
2023-04-07 11:03:24 +0100

    Moving to particle pool

---
+++ src/lib/effects/particles/particle-bank.js
@@ -0,0 +1,9 @@
+export const particleCount = 1000
+
+export const particleBank = []
+
+for (let i = 0; i < particleCount; i++) {
+  particleBank.push({
+    active: false
+  })
+}
--- src/lib/utils/random-variation.js
+++ src/lib/utils/random-variation.js
@@ -1,8 +1,6 @@
-const randomVariation = (start, range, int = false) => {
+export const randomVariation = (start, range, int = false) => {
   const variatedNumber = start + (Math.random() * range - range / 2)
 
   if (int) return Math.floor(variatedNumber)
   return variatedNumber
 }
-
-export { randomVariation }
